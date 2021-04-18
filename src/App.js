a28eb14f57cbb1095ea8533237ccf9817c119baf
kinakome <ryomatsu0120@gmail.com>
2021-04-18 22:57:39 +0900

    implement object state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 
 const App = (props) => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
+  const [state, setState] = useState(props);
+  const { name, price } = state;
 
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
   return (
     <>
       <p>
         現在の{name}は、{price}円です。
       </p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      {/* リアルタイムでフォームの内容をnameに適用する */}
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      {/* <button onClick={setState(props)}>Reset</button> こう書くと無限ループになる */}
+      {/* https://qiita.com/katsuomi/items/5a062dc1e4d0ab64ae21 */}
+      <button onClick={() => setState(props)}>Reset</button>
+      <input
+        value={name}
+        // スプレット構文を用いてnameだけ置き換え
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </>
   );
 };
