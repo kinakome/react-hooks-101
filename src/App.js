37cf1ca06c2c1b9bac0d148c1843ea7d1c5975e2
kinakome <ryomatsu0120@gmail.com>
2021-04-18 22:24:14 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>this is tamplate</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  // 引数に関数を設定できる、引数には現時点の状態が入る
+  const increment2 = () => setCount((previousCount) => previousCount + 1);
+  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+
+  const reset = () => setCount(0);
+  const double = () => setCount(count * 2);
+  const devideByThree = () =>
+    setCount((previousCount) =>
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
+
+  //トップレベルにdivを置きたくない場合はReact.Fragment => <></>と省略できる
+  return (
+    <>
+      <div> count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={devideByThree}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
