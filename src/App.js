f27ab397b6cf82cfdd76f9f1e9564dcbf8793f65
kinakome <ryomatsu0120@gmail.com>
2021-04-18 22:36:01 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment = () => {
-    setCount(count + 1);
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
   };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  // 引数に関数を設定できる、引数には現時点の状態が入る
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
-
-  const reset = () => setCount(0);
-  const double = () => setCount(count * 2);
-  const devideByThree = () =>
-    setCount((previousCount) =>
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    );
-
-  //トップレベルにdivを置きたくない場合はReact.Fragment => <></>と省略できる
   return (
     <>
-      <div> count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={devideByThree}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      {/* リアルタイムでフォームの内容をnameに適用する */}
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
+};
+
+App.defaultProps = {
+  name: "サンプル",
+  price: 1000,
 };
 
 export default App;
