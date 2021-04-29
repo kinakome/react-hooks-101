af91d30d783ee4b155034805c917d03937e9e943
kinakome <ryomatsu0120@gmail.com>
2021-04-29 18:01:35 +0900

    create operationLog table

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import EventForm from "./EventForm";
 import Events from "./Events";
+import OperationLogs from "./OperationLogs";
 import AppContext from "../contexts/AppContext";
 // indexは省略できる
 import reducer from "../reducers";
@@ -19,6 +20,7 @@ const App = () => {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   );
