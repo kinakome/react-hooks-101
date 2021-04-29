878121c8d4ba2de370c079a9e69f00ea6ade2ca3
kinakome <ryomatsu0120@gmail.com>
2021-04-29 17:37:23 +0900

    create operation logs fix

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,13 +1,21 @@
 import React, { useContext } from "react";
 //アクションのtype名を、ひとつのファイルにまとめて定数として管理する
-import { DELETE_EVENT } from "../actions";
+import { DELETE_EVENT, ADD_OPERATION_LOG } from "../actions";
 import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utils";
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext);
   const id = event.id;
   const handleClickDeleteButton = () => {
     dispatch({ type: DELETE_EVENT, id });
+
+    //イベント追加時の操作ログを追加する
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: `イベント(id=${id})を削除しました。`,
+      operatedAt: timeCurrentIso8601(),
+    });
   };
   return (
     <tr>
